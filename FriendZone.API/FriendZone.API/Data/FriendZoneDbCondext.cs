f480d83465548f45d4efe54bbad8870e874aef8f
Rashedul Islam <rashed.tareq@live.com>
2019-07-16 21:27:51 +0600

    Initial Commit On FriendZone API & UI

---
+++ FriendZone.API/FriendZone.API/Data/FriendZoneDbCondext.cs
@@ -0,0 +1,19 @@
+﻿using FriendZone.API.Controllers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FriendZone.API.Data
+{
+    public class FriendZoneDbCondext : DbContext
+    {
+        public FriendZoneDbCondext(DbContextOptions<FriendZoneDbCondext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
